4228953f9d7692d9d7a2c4492da169360855ba65
강민우 <augustman@knou.ac.kr>
2021-12-03 07:23:54 +0900

    Added ssl files

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
+const fs = require('fs')
 module.exports = {
     // options...
     devServer: {
         disableHostCheck: true,
+        https: {
+            key: fs.readFileSync('src/assets/ssl/pub_www.inclutter.com.key'),
+            cert: fs.readFileSync('src/assets/ssl/cert_www.inclutter.com.crt'),
+            ca: fs.readFileSync('src/assets/ssl/rootca_www.inclutter.com.crt')
+        },
     },
 };
