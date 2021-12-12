c9425e5f7cc476dce997550029e9d760dab6fcd5
강민우 <augustman@knou.ac.kr>
2021-12-12 20:16:07 +0900

    Modified default component list group vue.js file.

--- src/layouts/default/DefaultComponentListGroup.vue
+++ src/layouts/default/DefaultComponentListGroup.vue
@@ -29,7 +29,7 @@ export default {
   components: {
     DefaultComponentListItem: () => import(
       /* webpackChunkName: "default-component-list-group" */
-      './DefaultComponentListGroup'
+      './DefaultComponentListItem'
     )
   },
   props: {
