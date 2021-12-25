3560fbbab1584909465d01e99e7a6769e93d074e
inclutter <augustman@knou.ac.kr>
2021-12-25 14:59:18 +0900

    Changed port number.

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,7 @@ module.exports = {
   // options...
   devServer: {
     disableHostCheck: true,
-    https: {
-      key: fs.readFileSync('src/assets/ssl/pub_www.inclutter.com.key'),
-      cert: fs.readFileSync('src/assets/ssl/cert_www.inclutter.com.crt'),
-      ca: fs.readFileSync('src/assets/ssl/rootca_www.inclutter.com.crt')
-    },
+    https: true
   },
   transpileDependencies: [
     'vuetify'
