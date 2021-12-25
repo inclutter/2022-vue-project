63224bc17e84811aefb3de6e95c734d9a3479cb3
inclutter <augustman@knou.ac.kr>
2021-12-25 16:26:34 +0900

    Revert "Changed port number."
    This reverts commit 3560fbbab1584909465d01e99e7a6769e93d074e.

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,11 @@ module.exports = {
   // options...
   devServer: {
     disableHostCheck: true,
-    https: true
+    https: {
+      key: fs.readFileSync('src/assets/ssl/pub_www.inclutter.com.key'),
+      cert: fs.readFileSync('src/assets/ssl/cert_www.inclutter.com.crt'),
+      ca: fs.readFileSync('src/assets/ssl/rootca_www.inclutter.com.crt')
+    },
   },
   transpileDependencies: [
     'vuetify'
